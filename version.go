2a7c742163905a13f89b14dcfdd45460e33b1541
Robert Doiel <rsdoiel@gmail.com>
2023-06-04 23:16:52 -0700

    publish

--- version.go
+++ version.go
@@ -12,7 +12,7 @@ const (
     ReleaseDate = "2023-06-04"
 
     // ReleaseHash, the Git hash when version.go was generated
-    ReleaseHash = "70d9449"
+    ReleaseHash = "cdae4cb"
 
     LicenseText = `
 Redistribution and use in source and binary forms, with or without
